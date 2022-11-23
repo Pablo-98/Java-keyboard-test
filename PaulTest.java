47bb5ce9078efd2fbf5eb9889e9b82e1b3239aa9
Pablo Hernandez <hpablo575@gmail.com>
2022-11-22 17:48:25 -0700

    first

---
+++ PaulTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class PaulTest{
+    // I am inside of PaulTest class
+    public static void main (String[] args) {
+        //I am inside of the main method
+        // Main method executes code 
+        //why does the first class name have to align with file name and the other classes dont.
+
+        String world;
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("can you write hello world for me?");
+
+        world = keyboard.nextLine();
+
+
+
+        //find a way to record user input 
+
+
+
+    }
+
+}
+
